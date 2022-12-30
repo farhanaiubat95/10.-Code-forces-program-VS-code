5d6b9aa803fdbb99552a8d7c318ad88f39574e94
Farhana Bente Islam <21303195@iubat.edu>
2022-12-30 14:42:45 +0600

    first commit

---
+++ Watermelon.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(1<=w && w<=100)
+    {
+       if(w%2==0 && w>2)
+       {
+        cout<<"YES"<<endl;
+       }else 
+       {
+        cout<<"NO"<<endl;
+       }
+    }
+    
+}
