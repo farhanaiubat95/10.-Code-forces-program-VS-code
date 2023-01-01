8f174efcbdb1035adb9d863dba095395fe06337c
Farhana Bente Islam <21303195@iubat.edu>
2023-01-01 21:55:39 +0600

    First commit

---
+++ Round.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[100],c=0,i;
+    cin>>n>>k;
+
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+    if(a[i]>=0 && a[i]<=100)
+    {
+        if(a[i]>=a[k-1] && a[i]>0)
+        {
+           c++;
+        }
+    }
+    }
+    cout<<c<<endl; 
+}
