cc451d88e7dd9cebcdb4aae7e556eb1609fa6323
Farhana Bente Islam <21303195@iubat.edu>
2023-01-02 19:51:34 +0600

    First commit

---
+++ Matrix.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int a=0,i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+             cin>>a;
+             if(a==1)
+             {
+                cout<<abs(i-3)+abs(j-3)<<endl;
+             }
+
+        }
+    }
+}
